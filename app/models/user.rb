19837d396786b61a6003e8bfa09bf4eed1a4f4c1
Sam Walker <sewalker191@gmail.com>
2019-07-01 22:00:35 -0700

    Bug fixes, add show.json.jbuilder for session

--- app/models/user.rb
+++ app/models/user.rb
@@ -4,7 +4,7 @@ class User < ApplicationRecord
 
     attr_reader :password
 
-    after_intialize :ensure_session_token
+    after_initialize :ensure_session_token
 
     def self.find_by_credentials(username, password)
         user = User.find_by(username: username)
