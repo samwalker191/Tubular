471b7aecfef8469277006355a29e5ec4a15396ed
Sam Walker <sewalker191@gmail.com>
2019-07-02 11:40:33 -0700

    Add SignInFormContainer

---
+++ frontend/components/session/SignInFormContainer.js
@@ -0,0 +1,18 @@
+import { connect } from 'react-redux';
+import SessionForm from '../session/SessionForm';
+import { signin } from '../../actions/session_actions';
+
+const mapSTP = state => {
+    return ({
+        errors: state.errors.session,
+        formType: 'Sign In'
+    });
+};
+
+const mapDTP = dispatch => {
+    return ({
+        action: user => dispatch(signin(user))
+    });
+};
+
+export default connect(mapSTP, mapDTP)(SessionForm);
