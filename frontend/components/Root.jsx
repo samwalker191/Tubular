d0f23905a6574a02c4cb24ae2952dfd54cf9163c
Sam Walker <sewalker191@gmail.com>
2019-07-02 11:06:11 -0700

    Add Root and UserAuth components

---
+++ frontend/components/Root.jsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import { Provider } from 'react-redux';
+import { HashRouter } from 'react-router-dom';
+import UserAuth from './UserAuth';
+
+const Root = ({ store }) => (
+    <Provider store={store}>
+        <HashRouter>
+            <UserAuth/>
+        </HashRouter>
+    </Provider>
+);
