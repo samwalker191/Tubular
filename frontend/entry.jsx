7345e1f5ae9183cc80153684489ab78a3bea0054
Sam Walker <sewalker191@gmail.com>
2019-07-01 20:36:05 -0700

    Setup entry file to render

---
+++ frontend/entry.jsx
@@ -0,0 +1,7 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+
+document.addEventListener("DOMContentLoaded", () => {
+    const root = document.getElementById('root');
+    ReactDOM.render(<h1>IT BEGINS</h1>, root)
+})
