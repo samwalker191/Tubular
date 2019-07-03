33fdd156a8a098a4bb8f76d2a7facbdfda6186a7
Sam Walker <sewalker191@gmail.com>
2019-07-03 15:31:48 -0700

    Add VideosController

--- app/controllers/api/videos_controller.rb
+++ app/controllers/api/videos_controller.rb
@@ -1,22 +1,45 @@
 class Api::VideosController < ApplicationController
-    def index(query)
-
+    def index(query = '')
+        formatted_query = '%' + query + '%'
+        @videos = Video.where('videos.title like ?', formatted_query)
     end
 
     def create
         @video = Video.new(video_params)
+        @video.video.attach(params[:video][:video_attach])
+
+        if @video.save
+            render :show
+        else
+            render json: @video.errors.full_messages, status: 422
+        end
     end
 
     def show
-
+        @video = Video.find(params[:id])
+        render :show
     end
 
     def update
-
+        @video = Video.find(params[:id])
+        if current_user.id == @video.id 
+            if @video.update_attributes(video_params)
+                render: show
+            else
+                render json: @video.errors.full_messages, status: 422
+            end
+        else
+            render json: ['Must be owner of video to edit'], status: 422
+        end
     end
 
     def destroy
-
+        @video = Video.find(params[:id])
+        if current_user.id == @video.id
+            @video.destroy
+        else
+            render json: ['Must be owner of video to destroy'], status: 422
+        end
     end
 
     private
