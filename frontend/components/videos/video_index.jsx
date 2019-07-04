93f8b5c852bc075a3d2371a7c3b18f33d339b6c6
Sam Walker <sewalker191@gmail.com>
2019-07-04 09:30:14 -0700

    Add VideoIndex component

--- frontend/components/videos/video_index.jsx
+++ frontend/components/videos/video_index.jsx
@@ -2,6 +2,29 @@ import React from 'react';
 
 class VideoIndex extends React.Component {
 
+
+    componentDidMount() {
+        this.props.fetchVideos();
+    }
+
+    render() {
+        let videoItems = this.props.videos.map(video => <VideoIndexItem video={video} />)
+
+        return (
+            <div className='main-index'>
+                <div className='main-index-trending'>
+
+                </div>
+
+                <div className='main-index-recommended'>
+                    <h3>Recommended</h3>
+                    <ul className="main-index-recommended-list">
+                        {videoItems}
+                    </ul>
+                </div>
+            </div>
+        )
+    }
 }
 
 export default VideoIndex;
