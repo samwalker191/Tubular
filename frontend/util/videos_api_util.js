a78f6bbbc662d8c2fe7cdc19eac6ad052d99378e
Sam Walker <sewalker191@gmail.com>
2019-07-03 17:48:34 -0700

    Add videos_api_util

--- frontend/util/videos_api_util.js
+++ frontend/util/videos_api_util.js
@@ -1,7 +1,37 @@
 export const fetchVideos = query => (
     $.ajax({
         method: 'GET',
-        url: '/api/users',
+        url: '/api/videos',
         data: query
+    })
+);
+
+export const fetchVideo = videoId => (
+    $.ajax({
+        method: 'GET',
+        url: `/api/videos/${videoId}`
+    })
+);
+
+export const createVideo = video => (
+    $.ajax({
+        method: 'POST',
+        url: '/api/videos',
+        data: { video }
+    })
+);
+
+export const updateVideo = video => (
+    $.ajax({
+        method: 'PATCH',
+        url: `/api/videos/${video.id}`,
+        data: { video }
+    })
+);
+
+export const deleteVideo = videoId => (
+    $.ajax({
+        method: 'DELETE',
+        url: `/api/videos/${videoId}`
     })
 );
