8196929f483f447a82766b4551a1243e919588a2
Sam Walker <sewalker191@gmail.com>
2019-07-08 10:59:35 -0700

    Change likes controller into api namespace

--- app/controllers/api/likes_controller.rb
+++ app/controllers/api/likes_controller.rb
@@ -1,4 +1,4 @@
-class LikesController < ApplicationController
+class Api::LikesController < ApplicationController
     before_action :require_logged_in, only: [:create, :update, :destroy]
 
     def create
--- test/controllers/api/likes_controller_test.rb
+++ test/controllers/api/likes_controller_test.rb
@@ -1,6 +1,6 @@
 require 'test_helper'
 
-class LikesControllerTest < ActionDispatch::IntegrationTest
+class Api::LikesControllerTest < ActionDispatch::IntegrationTest
   # test "the truth" do
   #   assert true
   # end
