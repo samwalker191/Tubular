028e9a6a21b7a6715ffdec9a14393154fef0a5de
Sam Walker <sewalker191@gmail.com>
2019-07-10 16:52:00 -0700

    Tweak styling of app

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2019_07_08_233238) do
+ActiveRecord::Schema.define(version: 2019_07_10_202116) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -34,6 +34,14 @@ ActiveRecord::Schema.define(version: 2019_07_08_233238) do
     t.string "checksum", null: false
     t.datetime "created_at", null: false
     t.index ["key"], name: "index_active_storage_blobs_on_key", unique: true
+  end
+
+  create_table "channels", force: :cascade do |t|
+    t.integer "owner_id", null: false
+    t.text "description", default: "No description", null: false
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["owner_id"], name: "index_channels_on_owner_id", unique: true
   end
 
   create_table "comments", force: :cascade do |t|
