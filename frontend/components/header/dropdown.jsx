541b5c01263ebbe3f95fa713602f83e2b98401cf
Sam Walker <sewalker191@gmail.com>
2019-08-12 16:15:50 -0700

    Add user info to dropdown box

--- frontend/components/header/dropdown.jsx
+++ frontend/components/header/dropdown.jsx
@@ -4,13 +4,23 @@ import { faYoutube } from '@fortawesome/free-brands-svg-icons';
 import { faBars, faUserCircle, faVideo, faSearch } from '@fortawesome/free-solid-svg-icons';
 
 class Dropdown extends React.Component {
+    constructor(props) {
+        super(props)
 
+    }
 
     render() {
         return (
             <div className='dropdown-box'>
                 <div className='dropdown-box-header'>
-
+                    <div className='dropdown-box-header-icon'>
+                        {this.props.users[this.props.currentUserId]
+                            .username.slice(0, 1).toUpperCase()}
+                    </div>
+                    <div className='dropdown-box-header-user'>
+                        <span>{this.props.users[this.props.currentUserId].username}</span>
+                        <span>{this.props.users[this.props.currentUserId].email}</span>
+                    </div>
                 </div>
                 
                 <div className='dropdown-box-main'>
