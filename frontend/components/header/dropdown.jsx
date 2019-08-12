8d0ce98f4ac96074e2fb0e3f55ebaf69aa9dcbc3
Sam Walker <sewalker191@gmail.com>
2019-08-12 15:11:49 -0700

    Refactor z-indexes, add basic dropdown box to profile circle

---
+++ frontend/components/header/dropdown.jsx
@@ -0,0 +1,24 @@
+import React from 'react';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { faYoutube } from '@fortawesome/free-brands-svg-icons';
+import { faBars, faUserCircle, faVideo, faSearch } from '@fortawesome/free-solid-svg-icons';
+
+class Dropdown extends React.Component {
+
+
+    render() {
+        return (
+            <div className='dropdown-box'>
+                <div className='dropdown-box-header'>
+
+                </div>
+                
+                <div className='dropdown-box-main'>
+
+                </div>
+            </div>
+        );
+    }
+}
+
+export default Dropdown;
--- frontend/components/header/header.jsx
+++ frontend/components/header/header.jsx
@@ -3,17 +3,19 @@ import { Link } from 'react-router-dom';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 import { faYoutube } from '@fortawesome/free-brands-svg-icons';
 import { faBars, faUserCircle, faVideo, faSearch } from '@fortawesome/free-solid-svg-icons';
+import Dropdown from './dropdown';
 
 class Header extends React.Component {
     constructor(props) {
         super(props);
 
-        this.state = { search: ''}
+        this.state = { search: '', dropdownBoxClassName: 'hidden' }
 
         this.handleLogout = this.handleLogout.bind(this);
         this.handleInput = this.handleInput.bind(this);
         this.handleSearch = this.handleSearch.bind(this);
-        this.handleToggle = this.handleToggle.bind(this);
+        this.toggleSidebar = this.toggleSidebar.bind(this);
+        this.toggleDropdown = this.toggleDropdown.bind(this);
     }
 
     handleLogout() {
@@ -22,15 +24,23 @@ class Header extends React.Component {
 
     handleSearch() {
         this.props.history.push(`/search/${this.state.search}`);
-        this.setState({ search: ''});
+        this.setState({ search: '' });
     }
 
     handleInput(e) {
         this.setState({ search: e.currentTarget.value });
     }
 
-    handleToggle() {
+    toggleSidebar() {
         this.props.toggleSidebar();
+    }
+
+    toggleDropdown() {
+        if (this.state.dropdownBoxClassName === 'hidden') {
+            this.setState({ dropdownBoxClassName: '' })
+        } else {
+            this.setState({ dropdownBoxClassName: 'hidden' })
+        }
     }
 
     render() {
@@ -41,7 +51,7 @@ class Header extends React.Component {
                             <span>SIGN IN</span>
                         </Link>
         } else {
-            authButton = <button className='header-logout' onClick={this.handleLogout}>
+            authButton = <button className='header-logout' onClick={this.toggleDropdown}>
                             <div>
                                 {this.props.users[this.props.currentUserId]
                                 .username.slice(0,1).toUpperCase()}
@@ -60,7 +70,7 @@ class Header extends React.Component {
         return (
             <div className='header'>
                 <div className='header-left'>
-                    <button className='header-burger-button' onClick={this.handleToggle}>
+                    <button className='header-burger-button' onClick={this.toggleSidebar}>
                         <FontAwesomeIcon icon={faBars}/>
                     </button>
                     <Link to='/' className='header-logo'>
@@ -89,6 +99,13 @@ class Header extends React.Component {
                         <FontAwesomeIcon icon={faVideo} />
                     </Link>
                     {authButton}
+                    <div className={`dropdown-box-container ${this.state.dropdownBoxClassName}`}>
+                        <Dropdown 
+                            logout={this.props.logout}
+                            currentUserId={this.props.currentUserId}
+                            users={this.props.users}    
+                        />
+                    </div>
                 </div>
             </div>
         );
