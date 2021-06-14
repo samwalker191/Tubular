44347698f12108903821a5ab7657aa9eca8445d4
samwalker191 <sewalker191@gmail.com>
2021-06-13 23:41:56 -0700

    Add logger if dev, remove if prod

--- frontend/store/store.js
+++ frontend/store/store.js
@@ -3,11 +3,18 @@ import thunk from 'redux-thunk';
 import logger from 'redux-logger';
 import rootReducer from '../reducers/root_reducer';
 
+let middlewares;
+if (process.env.NODE_ENV === 'production') {
+    middlewares = [thunk];
+} else {
+    middlewares = [thunk, logger];
+}
+
 const configureStore = (preloadedState = {}) => (
     createStore(
         rootReducer,
         preloadedState,
-        applyMiddleware(thunk, logger)
+        applyMiddleware(...middlewares)
     )
 );
 
