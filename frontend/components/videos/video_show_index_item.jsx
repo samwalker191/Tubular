670a2d841c322edb6f92ee490de629a060494d9d
samwalker191 <sewalker191@gmail.com>
2021-06-14 22:28:36 -0700

    Update styling for video show index

--- frontend/components/videos/video_show_index_item.jsx
+++ frontend/components/videos/video_show_index_item.jsx
@@ -1,23 +1,25 @@
 import React from 'react';
 import { Link } from 'react-router-dom';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { faCircle } from '@fortawesome/free-solid-svg-icons';
 
-class VideoShowIndexItem extends React.Component {
 
-    render() {
+const VideoShowIndexItem = ({ video }) => {
 
-        return (
-            <li className='video-show-list-item'>
-                <Link to={`/watch/${this.props.video.id}`} className='video-show-list-link'>
-                    <p className='video-show-list-link-img-container'><img src={this.props.video.thumbnail}></img></p>
-                    <div className='video-show-index-details'>
-                        <h3>{this.props.video.title}</h3>
-                        <p>{this.props.video.owner}</p>
-                        <p>{this.props.video.views} Views</p>
-                    </div>
-                </Link>
-            </li>
-        );
-    }
+    return (
+        <Link to={`/watch/${video.id}`} className='video-show-list-link'>
+            <img src={video.thumbnail} />
+            <div className='video-show-index-details'>
+                <h3>{video.title}</h3>
+                <p>{video.owner}</p>
+                <div>
+                    <span>{video.views} Views</span>
+                    <FontAwesomeIcon icon={faCircle} className='sub-details-dot' />
+                    <span>{video.published}</span>
+                </div>
+            </div>
+        </Link>
+    );
 }
 
 export default VideoShowIndexItem;
